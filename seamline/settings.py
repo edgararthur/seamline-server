475b8c54985e74fccc402a8ce87712410a0f0924
edgararthur <gilfoyle.arthur@protonmail.com>
2025-01-27 13:31:15 +0000

    downgraded the pycopg2-binary

--- seamline/settings.py
+++ seamline/settings.py
@@ -159,7 +159,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # REST Framework settings
