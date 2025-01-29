ce8e473c7d5714760350f969aa1ac3829e65e46a
Edward Edgar Arthur <108295488+edgararthur@users.noreply.github.com>
2025-01-29 11:46:45 +0000

    Update wsgi.py

--- seamline/wsgi.py
+++ seamline/wsgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seamline.settings')
 
 application = get_wsgi_application()
 
+# Alias `application` to `app` for Vercel compatibility
 app = application
